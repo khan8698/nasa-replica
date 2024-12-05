288ef5b4cb8743ad5de8018a7aba1d3fdf33e588
Muhammad Ali Khan <khan8698@gmail.com>
2024-12-05 21:06:03 +0500

    feat: nasa more images section added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-const swiper = new Swiper(".swiper-container", {
+const mainSwiper = new Swiper(".swiper-container-main", {
   // Optional parameters
   grabCursor: true,
   spaceBetween: 0,
   slidesPerView: 1,
   loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const LatestNewsSwiper = new Swiper(".swiper-container-latest-new", {
+  // Optional parameters
+  grabCursor: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+  spaceBetween: 4,
+  slidesPerView: 1,
+  loop: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+
+    992: {
+      slidesPerView: 3,
+    },
+  },
   pagination: {
     el: ".swiper-pagination",
   },

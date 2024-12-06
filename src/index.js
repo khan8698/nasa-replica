f0999d4f8fbdca43c19b7d805714ad1fe54fdd20
Muhammad Ali Khan <khan8698@gmail.com>
2024-12-06 21:19:36 +0500

    feat: video added for home page + swiper added for missions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Main Swiper
 const mainSwiper = new Swiper(".swiper-container-main", {
   // Optional parameters
   grabCursor: true,
@@ -13,6 +14,7 @@ const mainSwiper = new Swiper(".swiper-container-main", {
   },
 });
 
+// Latest News Swiper
 const LatestNewsSwiper = new Swiper(".swiper-container-latest-new", {
   // Optional parameters
   grabCursor: true,
@@ -32,6 +34,26 @@ const LatestNewsSwiper = new Swiper(".swiper-container-latest-new", {
       slidesPerView: 3,
     },
   },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Missions Swiper
+const missionsSwiper = new Swiper(".swiper-container-missions", {
+  // Optional parameters
+  grabCursor: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+  spaceBetween: 0,
+  slidesPerView: 1,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
   },
